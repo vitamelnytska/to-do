342fc16842a75f9d04f233d690b18a17d6aa10ed
vitamelnytska <vita.melnytska@gmail.com>
2022-05-21 10:05:07 +0300

    add firebase config and others unsaved files

---
+++ config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDOTjWGd7k3CkuJg_Bn0H_5r2lS7Z2wfec',
+  authDomain: 'to-do-6a01c.firebaseapp.com',
+  projectId: 'to-do-6a01c',
+  storageBucket: 'to-do-6a01c.appspot.com',
+  messagingSenderId: '673079827779',
+  appId: '1:673079827779:web:692a3243bacca712c623f6',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
