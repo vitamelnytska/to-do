2cb523ac004591a8c48ef658a1e56be8f31eab6b
vitamelnytska <vita.melnytska@gmail.com>
2022-05-21 21:43:04 +0300

    fixing after the lesson

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
 export class CreateUserDto {
   readonly name: string;
-  readonly surname: number;
+  readonly surname: string;
 }
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,5 +1,4 @@
 export class UpdateUserDto {
-  readonly title: string;
-  readonly priority: number;
-  readonly duration: number;
+  readonly name: string;
+  readonly surname: string;
 }
