dc9187a8c08ee977ffd431bbcd27d308f13d1cea
vitamelnytska <vita.melnytska@gmail.com>
2022-05-21 15:47:23 +0300

    replace app files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TasksModule } from '../tasks/tasks.module';
-import { UsersModule } from '../users/users.module';
+import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [TasksModule, UsersModule],
