5408f4eace8eac28cccc46c1cb3b3b103f5753e0
vitamelnytska <vita.melnytska@gmail.com>
2022-05-25 22:39:40 +0300

    add authz module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
+import { AuthzModule } from './authz/authz.module';
 
 @Module({
-  imports: [TasksModule, UsersModule],
+  imports: [TasksModule, UsersModule, AuthzModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,12 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getPrivate(): string {
+    return 'This is a public resource. Welcome visitor!';
+  }
+
+  getPublic(): string {
+    return 'This is a protected resource. Welcome member';
+  }
 }
