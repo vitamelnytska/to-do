cb79ddfee3e47c4fe1124c36c92defc445275b79
vitamelnytska <vita.melnytska@gmail.com>
2022-05-28 14:47:30 +0300

    add user schema abd crud operations for user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import {InjectModel} from "@nestjs/mongoose";
+import { InjectModel } from '@nestjs/mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
-import {User, UserDocument} from "./schemas/users.schema";
-import {Model} from "mongoose";
-import {UpdateUserDto} from "./dto/update-user.dto";
+import { User, UserDocument } from './schemas/users.schema';
+import { Model } from 'mongoose';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>){}
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
   async getAll(): Promise<User[]> {
     return this.userModel.find().exec();
   }
